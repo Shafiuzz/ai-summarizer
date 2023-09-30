d6e549a39b1279da2cde04fde04b0102aac848ca
Shafiuzz <97677484+Shafiuzz@users.noreply.github.com>
2023-09-30 20:29:06 +0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-
+           <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+           </nav>
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
         <span className='blue_gradient '>OpenAI GPT-4</span>
